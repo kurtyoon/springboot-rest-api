37bb7ed81594a83cb5f3d3c951752be609db1c3a
kurtyoon <akakertcobain@naver.com>
2023-05-20 02:16:05 +0900

    first commit

---
+++ src/main/java/com/sbringboot/blog/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.sbringboot.blog.repository;
+
+import com.sbringboot.blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
---
+++ src/main/java/com/sbringboot/blog/service/PostService.java
@@ -0,0 +1,17 @@
+package com.sbringboot.blog.service;
+
+import com.sbringboot.blog.payload.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
